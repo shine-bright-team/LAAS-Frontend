04359d8e1cb0b17deecc9fcc4f4da8f0b439e497
fah <phurichayakhem1@gmail.com>
2023-06-08 16:49:38 +0700

    error

--- lib/components/Lender/Review.dart
+++ lib/components/Lender/Review.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:laas/components/Lender/Star.dart';
+import 'package:laas/config/theme/custom_color.g.dart';
 
 Widget review(BuildContext context) {
   return (Column(
@@ -48,11 +49,11 @@ Widget review(BuildContext context) {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const StarDisplayWidget(
+                     StarDisplayWidget(
                       value: 4,
-                      filledStar: Icon(Icons.star, color: Colors.yellow),
+                      filledStar: Icon(Icons.star, color: Theme.of(context).extension<CustomColors>()!.star)),
                       unfilledStar:
-                          Icon(Icons.star_border, color: Colors.yellow),
+                          Icon(Icons.star_border, color: Theme.of(context).extension<CustomColors>()!.star)),
                       key: null,
                     ),
                     const SizedBox(
