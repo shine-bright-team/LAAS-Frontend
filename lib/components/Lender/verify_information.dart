7ecfe2bcd153b06c10ccc904cd3b9d062f18d5ee
wtyaumm <muaymiimuay@gmail.com>
2023-06-23 01:53:47 +0700

    Edit kyc.dart

---
+++ lib/components/Lender/verify_information.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VerifyInformation extends StatefulWidget {
+  const VerifyInformation({super.key});
+
+  @override
+  State<VerifyInformation> createState() => _VerifyInformationState();
+}
+
+class _VerifyInformationState extends State<VerifyInformation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/kyc.dart
+++ lib/pages/kyc.dart
@@ -1,17 +1,257 @@
 import 'package:flutter/material.dart';
+import 'package:laas/components/Lender/verify_information.dart';
 
-class Kyc extends StatelessWidget {
+class Kyc extends StatefulWidget {
   const Kyc({super.key});
+
+  @override
+  State<Kyc> createState() => _KycState();
+}
+
+class _KycState extends State<Kyc> {
+  bool isAgreed = false;
+
+  void agree(bool? check) {
+    setState(() {
+      isAgreed = check!;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("LApprove"),
-      ),
-      body: const Center(
-        child: Text("Create Screen"),
+      // appBar: AppBar(
+      //   title: const Text("LApprove"),
+      // ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(50),
+              child: Column(
+                // mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const LinearProgressIndicator(
+                    value: 0.5,
+                  ),
+                  const SizedBox(
+                    height: 35,
+                  ),
+                  Text(
+                    "Terms and Agreement",
+                    style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.headlineSmall!.fontSize,
+                        color: Theme.of(context).colorScheme.primary),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Condition of use\n  • Lender need to pay subscription to use our application\n  • The subscription will cost 75 THB per Month\n  • The information you provide us must be legally true. Providing false information can lead to legal action.",
+                    style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.bodyMedium!.fontSize,
+                        color: Theme.of(context).colorScheme.secondary),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Privacy policy\n  • When using our service. We need to collect your information for KYC verification.",
+                    style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.bodyMedium!.fontSize,
+                        color: Theme.of(context).colorScheme.secondary),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Age restriction\n  • You must be at least 18 years of age before using our service.",
+                    style: TextStyle(
+                        fontSize:
+                            Theme.of(context).textTheme.bodyMedium!.fontSize,
+                        color: Theme.of(context).colorScheme.secondary),
+                  ),
+                  SizedBox(height: 100),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              color: Theme.of(context).colorScheme.background,
+              padding: const EdgeInsets.only(bottom: 100, left: 50, right: 50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Checkbox(value: isAgreed, onChanged: agree),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const Flexible(
+                    child: Text("I agree with the terms and agreement"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 40, left: 50, right: 50),
+              child: Row(
+                children: [
+                  const Expanded(child: SizedBox()),
+                  Expanded(
+                    child: FilledButton(
+                      onPressed: () {
+                        if (isAgreed) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const VerifyInformation()),
+                          );
+                        }
+                        print(isAgreed);
+                      },
+                      child: const Text("Next"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  Widget checkAgreeButton() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Checkbox(value: isAgreed, onChanged: agree),
+            const SizedBox(
+              width: 5,
+            ),
+            const Flexible(
+              child: Text("I agree with the terms and agreement"),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 22,
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Row(
+            children: [
+              const Expanded(child: SizedBox()),
+              Expanded(
+                child: FilledButton(
+                  onPressed: () {
+                    if (isAgreed) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const VerifyInformation()),
+                      );
+                    }
+                    print(isAgreed);
+                  },
+                  child: const Text("Next"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 }
+
+
+// Stack(
+//         children: [
+//           SingleChildScrollView(
+//             child: Container(
+//               margin: const EdgeInsets.all(50),
+//               child: Column(
+//                 mainAxisSize: MainAxisSize.max,
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   const LinearProgressIndicator(
+//                     value: 0.5,
+//                   ),
+//                   const SizedBox(
+//                     height: 35,
+//                   ),
+//                   Text(
+//                     "Terms and Agreement",
+//                     style: TextStyle(
+//                         fontSize:
+//                             Theme.of(context).textTheme.headlineSmall!.fontSize,
+//                         color: Theme.of(context).colorScheme.primary),
+//                   ),
+//                   const SizedBox(
+//                     height: 10,
+//                   ),
+//                   Text(
+//                     "Condition of use\n  • Lender need to pay subscription to use our application\n  • The subscription will cost 75 THB per Month\n  • The information you provide us must be legally true. Providing false information can lead to legal action.\nPrivacy policy\n  • When using our service. We need to collect your information for KYC verification.\nAge restriction\n  • You must be at least 18 years of age before using our service.",
+//                     style: TextStyle(
+//                         fontSize: Theme.of(context).textTheme.bodyMedium!.fontSize,
+//                         color: Theme.of(context).colorScheme.secondary),
+//                   ),
+//                   Align(
+//                       alignment: Alignment.bottomLeft,
+//                       child: Column(
+//                         children: [
+//                           Row(
+//                             children: [
+//                               Checkbox(value: isAgreed, onChanged: agree),
+//                               const SizedBox(
+//                                 width: 5,
+//                               ),
+//                               const Flexible(
+//                                 child: Text("I agree with the terms and agreement"),
+//                               ),
+//                             ],
+//                           ),
+//                           const SizedBox(
+//                             height: 22,
+//                           ),
+//                           Align(
+//                             alignment: Alignment.bottomRight,
+//                             child: Row(
+//                               children: [
+//                                 const Expanded(child: SizedBox()),
+//                                 Expanded(
+//                                   child: FilledButton(
+//                                     onPressed: () {
+//                                       print(isAgreed);
+//                                     },
+//                                     child: const Text("Next"),
+//                                   ),
+//                                 ),
+//                               ],
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
